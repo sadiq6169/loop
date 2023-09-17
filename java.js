e6f4070f86770198cc56225b4cf5afc96b127125
sadiq6169 <sadiq6169@gmail.com>
2023-09-17 19:48:15 +0500

    for loop

---
+++ java.js
@@ -0,0 +1,71 @@
+
+// let i = 1;
+// for( i; i <= 10; i++){
+//     console.log("Jadugar" , i)
+// }
+
+// let array = [20,80,30,40,90,95]
+// let number = 0;
+// for(number;number <= 10; number++){
+//    if(array[number] <= 80){
+//    console.log("you are failed", number)
+//    }
+//    else if(array[number] >= 80){
+//     console.log("You are passed " ,number)
+//    }
+// } 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
